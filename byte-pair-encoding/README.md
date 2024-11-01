0c71943f9d9453c2620d205d4adf2603610f1a6d
Jaime Lopez <jailop@protonmail.com>
2024-10-31 21:10:10 -0400

    leftist heap

--- byte-pair-encoding/README.md
+++ byte-pair-encoding/README.md
@@ -4,3 +4,4 @@
 
 * Sennrich, R. & Haddow, B. & Birch, A. (2016). Neural Machine Translation of
   Rare Words with Subword Units. _arXiv_ https://doi.org/10.48550/arXiv.1508.07909
+* HuggingFace (n.d.) _Byte-Pair Encoding tokenization_. https://huggingface.co/learn/nlp-course/chapter6/5
--- byte-pair-encoding/bpe.zig
+++ byte-pair-encoding/bpe.zig
@@ -1,5 +1,49 @@
 const std = @import("std");
 
+const Pair = struct {
+    a: [64]u8,
+    b: [64]u8,
+    freq: u32,
+    pub fn set(x: []const u8, y: []const u8, accu: u32) Pair {
+        var self : ?Pair = undefined;
+        std.mem.copyForwards(u8, self.?.a, x);
+        std.mem.copyForwards(u8, self.?.b, y);
+        self.?.freq = accu;
+        return self.?;
+    }
+    pub fn add(self: *Pair, accu: u32) void {
+        self.freq += accu;
+    }
+    pub fn equal(self: Pair, x: []const u8, y: []const u8) bool {
+        return std.mem.eql(u8, &(self.a), x) and std.mem.eql(u8, &(self.b), y);
+    }
+};
+
+fn updatePair(pairs: *std.ArrayList(Pair), x: []const u8, y: []const u8, accu: u32) !void {
+    for (0..pairs.items.len) | i | {
+        if (pairs.items[i].equal(x, y)) {
+            pairs.items[i].add(accu);
+            return;
+        }
+    }
+    try pairs.*.append(Pair.set(x, y, accu)); 
+}
+
+fn getPairs(allocator: std.mem.Allocator, vocab: std.StringHashMap(u32)) !std.ArrayList(Pair) {
+    var it = vocab.iterator();
+    var pairs = std.ArrayList(Pair).init(allocator);
+    _ = &pairs;
+    while (it.next()) | entry | {
+        var symbols = std.mem.splitScalar(u8, entry.key_ptr.*, ' ');
+        while (symbols.next()) | sym1 | {
+            const following = symbols.peek();
+            if (following) | sym2 | {
+                try updatePair(&pairs, sym1, sym2, entry.value_ptr.*);    
+            }
+        }
+    }
+}
+
 fn getStats(allocator: std.mem.Allocator, vocab: std.StringHashMap(u32)) !std.StringHashMap(u32) {
     var pairs = std.StringHashMap(u32).init(allocator);
     var it = vocab.iterator();
@@ -22,23 +66,22 @@ fn getStats(allocator: std.mem.Allocator, vocab: std.StringHashMap(u32)) !std.St
     return pairs;
 }
 
-fn mergeVocab(allocator: std.mem.Allocator, pair: []const u8, v_in: std.StringHashMap(u32)) !std.StringHashMap(u32) {
-    var v_out = std.StringHashMap(u32).init(allocator);
-    var escaped_pair = allocator.alloc(u8, pair.len + 1);
-    defer allocator.free(escaped_pair);
-    var idx_target = 0;
-    for (pair) | c | {
-        if (c == ' ') {
-            escaped_pair[idx_target] = '\\';
-            idx_target += 1;
-        }
-        escaped_pair[idx_target] = c;
-        idx_target += 1;
-    }
-    const pattern = std.fmt.allocPrint(allocator, "(?<!\\S){s}(?!\\S)", .{escaped_pair});
-
-    return v_out;
-}
+//fn mergeVocab(allocator: std.mem.Allocator, pair: []const u8, v_in: std.StringHashMap(u32)) !std.StringHashMap(u32) {
+//    var v_out = std.StringHashMap(u32).init(allocator);
+//    var escaped_pair = allocator.alloc(u8, pair.len + 1);
+//    defer allocator.free(escaped_pair);
+//    var idx_target = 0;
+//    for (pair) | c | {
+//        if (c == ' ') {
+//            escaped_pair[idx_target] = '\\';
+//            idx_target += 1;
+//        }
+//        escaped_pair[idx_target] = c;
+//        idx_target += 1;
+//    }
+//
+//    return v_out;
+//}
 
 fn printStringMap(sm: std.StringHashMap(u32)) void {
     var it = sm.iterator();
@@ -49,7 +92,7 @@ fn printStringMap(sm: std.StringHashMap(u32)) void {
 
 test "BPE" {
     const allocator = std.testing.allocator;
-    var vocab = std.StringHashMap(u32).init(std.heap.page_allocator);
+    var vocab = std.StringHashMap(u32).init(allocator);
     defer vocab.deinit();
 
     try vocab.put("l o w </w>", 5);
@@ -59,14 +102,7 @@ test "BPE" {
     
     const num_merges = 10;
     for (0..num_merges) |_| {
-        var pairs = try getStats(allocator, vocab);
-        defer {
-            var it = pairs.iterator();
-            while (it.next()) | entry | {
-                allocator.free(entry.key_ptr.*);
-            }
-            pairs.deinit();
-        }
-        printStringMap(pairs);
+        var pairs = try getPairs(allocator, vocab);
+        defer pairs.deinit();
     }
 }
